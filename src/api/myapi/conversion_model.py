b570ccf563b6acefd62c8f492b8bfe6d227481b4
selinaweh <selinaweh11@gmail.com>
2024-02-26 17:13:33 +0100

    modified model

--- src/api/myapi/conversion_model.py
+++ src/api/myapi/conversion_model.py
@@ -1,5 +1,7 @@
-from pydantic import BaseModel
-
+from pydantic import BaseModel, Field
+from typing import Optional
 
 class ConversionResponse(BaseModel):
     filename: str
+    content_type: str
+    content: bytes = Field(..., description="The content of the converted file as a byte string.")
