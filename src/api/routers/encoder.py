3d73e72cf6f35f579032c431cf105f51d52af112
selinaweh <selinaweh11@gmail.com>
2024-02-28 08:57:38 +0100

    encoder service works now

--- src/api/routers/encoder.py
+++ src/api/routers/encoder.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, UploadFile, File, HTTPException, Form
+from fastapi import APIRouter, UploadFile, File, HTTPException, Body
 import base64
 from src.api.myapi.conversion_model import ConversionResponse, InputModel
 from src.service.encoder_service import convert_audio
@@ -10,18 +10,16 @@ router = APIRouter(
 
 
 @router.post("/convert", response_model=ConversionResponse)
-def convert_audio_endpoint(input_model: InputModel, file: UploadFile = File(...)):      #src_format: str, target_format: str
+def convert_audio_endpoint(input_model: InputModel = Body(...), file: UploadFile = File(..., media_type="audio/mpeg")):
     valid_target_formats = ["wav", "flac", "ogg"]
     if input_model.target_format not in valid_target_formats:
         raise HTTPException(status_code=400, detail=UNSUPPORTED_FORMAT_ERROR)
 
     try:
         input_data = file.file.read()
-        print(f"input_data:{input_data}")
         output_data = convert_audio(input_data, input_model.src_format, input_model.target_format)
-        print(f"output_data: {output_data}")
         base64_encoded_output = base64.b64encode(output_data).decode('utf-8')
-        conversion_response = ConversionResponse(file_type=input_model.target_format, content=base64_encoded_output)
+        conversion_response = ConversionResponse(file_type=input_model.target_format, file_data=base64_encoded_output)
         return conversion_response
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"{FILE_CONVERSION_ERROR}: {str(e)}")
